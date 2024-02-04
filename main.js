ac857db5b7a6053b76a2a401875899692f7a8a44
Dinesh Reddy <dinu350930@gmail.com>
2024-02-04 17:19:30 +0530

    04-02-2024

--- main.js
+++ main.js
@@ -5,3 +5,25 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true
 });
+
+function rain() {
+    let amount = 20;
+    let projectSection = document.getElementById('projects');
+  
+    for (let i = 0; i < amount; i++) {
+      let raindrop = document.createElement('div');
+      raindrop.className = 'rain';
+  
+      let posX = Math.floor(Math.random() * window.innerWidth);
+      let duration = Math.random() * 2 + 1; // Adjust the falling speed
+  
+      raindrop.style.left = posX + 'px';
+      raindrop.style.animationDuration = duration + 's';
+  
+      projectSection.appendChild(raindrop);
+    }
+  }
+  
+  // Call the rain function
+  rain();
+  
